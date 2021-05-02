4d27f91965a85bc92e43566770518c37af4c6fa7
Luan Duarte <luansouza789@gmail.com>
2021-05-01 23:31:57 -0300

    finish challenge

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -8,9 +8,16 @@ import {
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
+import { IconType } from 'react-icons';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface IInputProps {
+  name: string;
+  icon?: IconType;
+  placeholder: string;
+}
+
+const Input = ({ name, icon: Icon, ...rest }: IInputProps) => {
+  const inputRef = useRef<HTMLInputElement >(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,52 @@
+import { ReactNode, useEffect, useState } from 'react';
+import ReactModal from 'react-modal';
+
+interface IModalProps {
+  isOpen: boolean;
+  children: ReactNode;
+  setIsOpen: () => void;
+}
+
+function Modal({ children, isOpen, setIsOpen }: IModalProps) {
+  const [modalStatus, setModalStatus] = useState(false);
+
+  useEffect(() => {
+
+    if (isOpen) {
+      setModalStatus(true);
+    }else{
+      setModalStatus(false);
+    }
+  }, [isOpen]);
+
+  return (
+    <ReactModal
+      shouldCloseOnOverlayClick={!false}
+      onRequestClose={setIsOpen}
+      isOpen={modalStatus}
+      ariaHideApp={false}
+      style={{
+        content: {
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+          background: '#F0F0F5',
+          color: '#000000',
+          borderRadius: '8px',
+          width: '736px',
+          border: 'none',
+        },
+        overlay: {
+          backgroundColor: '#121214e6',
+        },
+      }}
+    >
+      {children}
+    </ReactModal>
+  );
+};
+
+export default Modal;
